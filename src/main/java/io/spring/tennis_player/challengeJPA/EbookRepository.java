2efb89d38f03fbd551d3b5a921fbfba504544a84
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2025-05-24 00:44:28 +0300

    Adding Challenge solution with two-way of implementing it

---
+++ src/main/java/io/spring/tennis_player/challengeJPA/EbookRepository.java
@@ -0,0 +1,40 @@
+package io.spring.tennis_player.challengeJPA;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class EbookRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void deleteEbookById(int id) {
+        Ebook ebook = entityManager.find(Ebook.class, id);
+        entityManager.remove(ebook);
+    }
+
+    public Ebook updateEbook(Ebook ebook) {
+        return entityManager.merge(ebook);
+    }
+
+    public Ebook insertEbook(Ebook ebook) {
+        return entityManager.merge(ebook);
+    }
+
+    public Ebook getEbookById(int id) {
+        return entityManager.find(Ebook.class, id);
+    }
+
+    public List<Ebook> getAllEbooks() {
+        TypedQuery<Ebook> query = entityManager.createNamedQuery("get_all_ebooks", Ebook.class);
+
+        return query.getResultList();
+    }
+}
